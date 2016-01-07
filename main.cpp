e67f27d2e3f2df3764e740629f42bf662041d175
Lance Matysik <lancematysik@gmail.com>
2016-01-06 19:49:03 -0600

    fixed infinite loop

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <complex>
+#include <iostream>
 
 const int WINDOW_HEIGHT = 800;
 const int WINDOW_WIDTH = 800;
@@ -16,50 +17,53 @@ int main() {
   if (!font.loadFromFile("res/Munro.ttf")) {
     return 1;
   }
+  
+  long double pixelReal, pixelImaginary;
+  std::complex<long double> oldZ, newZ;
+  std::complex<long double> c;
 
-  while (window.isOpen()) {
+  #pragma omp for ordered schedule(dynamic) collapse(2)
+  for (int x = 0; x < WINDOW_WIDTH; x++) {
+    for (int y = 0; y < WINDOW_HEIGHT; y++) {
+
+      pixelReal = 1.5 * (x - (WINDOW_WIDTH / 2)) / (0.5 * WINDOW_WIDTH);
+      pixelImaginary = (y - (WINDOW_HEIGHT / 2)) / (0.5 * WINDOW_HEIGHT);
+      newZ = std::complex<long double>(0, 0);
+      c = std::complex<long double>(pixelReal, pixelImaginary);
+
+      int i;
+
+      #pragma omp ordered
+      for (i = 0; i < MAX_ITER; i++) {
+	oldZ = newZ;
+	newZ = oldZ * oldZ + c;
+	if ((newZ.real() * newZ.real() + newZ.imag() * newZ.imag()) > 4) {
+	  break;
+	}
+      }
+	
+      vertex.position = sf::Vector2f(x, y);
+
+      if (i == MAX_ITER) {
+	vertex.color = sf::Color::Green;
+      } else {
+	vertex.color = sf::Color::Black;
+      }
+	
+      vertexArray.append(vertex);
+	
+    }
+
+  }
+ 
+    while (window.isOpen()) {
+
     sf::Event event;
-
+    
     while (window.pollEvent(event)) {
 
       if (event.type == sf::Event::Closed) {
 	window.close();
-      }
-
-    }
-
-    long double pixelReal, pixelImaginary;
-    std::complex<long double> oldZ, newZ;
-    std::complex<long double> c;
-    int i;
-
-    #pragma omp for ordered schedule(dynamic) collapse(2)
-    for (int x = 0; x < WINDOW_WIDTH; x++) {
-      for (int y = 0; y < WINDOW_HEIGHT; y++) {
-
-	pixelReal = 1.5 * (x - (WINDOW_WIDTH / 2)) / (0.5 * WINDOW_WIDTH);
-	pixelImaginary = (y - (WINDOW_HEIGHT / 2)) / (0.5 * WINDOW_HEIGHT);
-	newZ = std::complex<long double>(0, 0);
-	c = std::complex<long double>(pixelReal, pixelImaginary);
-
-	#pragma omp ordered
-	for (i = 0; i < MAX_ITER; i++) {
-	  oldZ = newZ;
-	  newZ = oldZ * oldZ + c;
-	  if ((newZ.real() * newZ.real() + newZ.imag() * newZ.imag()) > 4) {
-	    break;
-	  }
-	}
-	
-	vertex.position = sf::Vector2f(x, y);
-	
-	if (i == MAX_ITER) {
-	  vertex.color = sf::Color::Green;
-	} else {
-	  vertex.color = sf::Color::Black;
-	}
-      
-	vertexArray.append(vertex);
       }
 
     }
@@ -69,12 +73,11 @@ int main() {
     text.setCharacterSize(24);
     text.setColor(sf::Color::Red);
 
-    window.clear();
     window.draw(vertexArray);
-    vertexArray.clear();
     window.draw(text);
     window.display();
-
+    
   }
-  return 0;
+
+    return 0;
 }
